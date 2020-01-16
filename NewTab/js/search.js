1424f738b496852bdc42cac1e80a2cd9fdb27864
AhkunTa <gqk1213@foxmail.com>
2020-01-16 11:22:58 +0800

    优化输入选择

--- NewTab/js/search.js
+++ NewTab/js/search.js
@@ -89,6 +89,8 @@ function eventListeners() {
         } else if (event.keyCode == 38 || event.keyCode == 40 || event.keyCode == 37 || event.keyCode == 39) {
             return
         }
+        chooseItem = 0;
+        isfirst = 1;
         if (indexValue === '05') {
             jsonPURL = url.jsonPLink.doge + word;
         }
